0a08818fab2fc255caa44ed5b84c15a6ef92c3b3
shalini39 <sk39happy@gmail.com>
2024-08-05 13:58:41 +0530

    added a callback method in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
   validates_presence_of :first_name, :lastname, :email
+  before_create :display_message
+
+  def display_message
+    p 'hello, this is a message before create action'
+  end
 end
