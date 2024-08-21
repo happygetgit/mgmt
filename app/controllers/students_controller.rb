27eb7462fee54cb972a744ca2384e9a838993e7e
shalini39 <sk39happy@gmail.com>
2024-08-21 16:47:00 +0530

    bootstrap admin theme integration

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  layout 'admin'
   before_action :set_student, only: %i[show edit update destroy]
   def index
     @students = Student.all
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "welcomes#index"
   resources :students
+
+  namespace :admin do
+    get 'dashboard' => 'dashboard#index'
+  end
 end
